e46f6d86b680a9f66ebca4ef933e0ab181c5f82b
Jeffrey Marvin Forones <aiscenblue@gmail.com>
2017-10-28 21:43:23 +0800

    enhancement

--- start.py
+++ start.py
@@ -1,8 +1,7 @@
-import sys
 from flask_app_core import Bootstrap
 from app.config.app import DevelopmentConfig
 
 
 if __name__ == "__main__":
-    bootstrap = Bootstrap(__name__, __file__, sys.argv, config=DevelopmentConfig)
+    bootstrap = Bootstrap(import_name=__name__, app_dir=__file__, config=DevelopmentConfig, environment='development')
     bootstrap.start()
