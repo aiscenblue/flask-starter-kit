46f3c5f90ec59fc63043cb2e29b19e6cc6d094b4
Jeffrey Marvin Forones <aiscenblue@gmail.com>
2018-10-19 07:55:20 +0800

    code cleanup

--- services/uploader.py
+++ services/uploader.py
@@ -1,6 +1,2 @@
-import os
-from flask import Flask, request, redirect, url_for
-from werkzeug.utils import secure_filename
-
 UPLOAD_FOLDER = '/path/to/the/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
